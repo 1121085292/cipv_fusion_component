e251afb334171ba8070fcc8883b0f77c1a9ab203
tang <122545505+1121085292@users.noreply.github.com>
2023-07-17 13:48:44 +0800

    Update radar_detection_component.cc

--- component/radar_detection_component.cc
+++ component/radar_detection_component.cc
@@ -9,47 +9,65 @@ bool RadarDetectionComponent::Init()
 {   
     // seq_num = 0.0f;
     //init publisher
-    writer_ = ComponentBase::node_->CreateWriter<RadarState>("/perception/radar_state");
-    camera_writer_ = node_->CreateWriter<LeadDataV3>("/perception/camera");
-    car_writer_ = node_->CreateWriter<CarState>("/car_state");
+    fusion_writer_ = ComponentBase::node_->CreateWriter<RadarState>("/perception/output/cipv");
+    // camera_writer_ = node_->CreateWriter<LeadDataV3>("/perception/camera");
+    // car_writer_ = node_->CreateWriter<CarState>("/car_state");
     
     return true;
 }
 
-bool RadarDetectionComponent::Proc(const std::shared_ptr<LeadDataV3>& camera_obj,
-                                const std::shared_ptr<RadarState>& radar_obj,
+bool RadarDetectionComponent::Proc(const std::shared_ptr<LeadsV3>& camera,
+                                const std::shared_ptr<RadarState>& radar,
                                 const std::shared_ptr<CarState>& car_info)
 {       
-    //read message
-    // 1.repeated
-    // int size = radar_obj->can_mono_times_size();
-    // for(size_t i = 0; i < size; ++i){
-    //     radar_can_mono_times = radar_obj->can_mono_times(i);
-    // }
-
-    uint64_t radar_md_mono_time = radar_obj->md_mono_time();
-    uint64_t car_state_mono_time = radar_obj->car_state_mono_time();
-
-    // //class LeadData
-    // uint64_t d = radar_obj_info.d_rel();
-    // uint64_t y = radar_obj_info.y_rel();
-    float i = 0.8f;
-    float j = 50.2f;
-
-    // writer
-    auto out_msg1 = std::shared_ptr<RadarState>();
-    auto out_msg2 = std::shared_ptr<LeadDataV3>();
-    auto out_msg3 = std::shared_ptr<CarState>();
-    // out_msg->add_can_mono_times(radar_can_mono_times);
-    out_msg1->set_md_mono_time(radar_md_mono_time);
-    out_msg1->set_md_mono_time(car_state_mono_time);
-    out_msg2->set_prob(i);
-    out_msg3->set_v_ego(j);
-    // out_msg->mutable_lead_one()->CopyFrom(radar_obj_info);  
-
-    writer_->Write(out_msg1);
-    camera_writer_->Write(out_msg2);
-    car_writer_->Write(out_msg3);
-    return true;
+    // AINFO << "Enter cipv fusion component, message timestamps:"
+    //       << radar->md_mono_time() << "current timestamps:"
+    //       << 
+    bool status = InternalProc(camera, radar, car_info, out_message);
+    if (status) {
+        fusion_writer_->Write(out_msg);
+        AINFO << "Send cipv output message.";
+    }
+    return status;
 }
 
+// template<calss T>
+// void ReadMessage(const std::shared_ptr<T>& obj) {
+    
+// }
+
+bool RadarDetectionComponent::InternalProc(const std::shared_ptr<LeadsV3>& camera,
+                                const std::shared_ptr<RadarState>& radar,
+                                const std::shared_ptr<CarState>& car_info,
+                                std::shared_ptr<RadarState>& out_msg) {
+    //1.解析数据
+    // camera
+    // float prob = camera->prob();
+    // std::vector<float> camera_x;
+    // int size = camera->x_size();
+    // for(int i = 0; i < size; ++i){
+    //     float x = camera->x(i);
+    //     camera_x.push_back(x);
+    // }
+    
+    // // repeated value
+    // int size = radar->can_mono_times_size();
+    // for(int i = 0; i < size; ++i){
+    //     out_msg->set_can_mono_times(i);
+    // }
+    // optional value
+    uint64_t radar_md_mono_time = radar->md_mono_time();
+    uint64_t car_state_mono_time = radar->car_state_mono_time();
+
+    // class  
+    // cipv_fusion_component::proto::LeadData& radar_obj;
+    // radar_obj.
+    // writer
+    // auto out_msg = std::shared_ptr<RadarState>();
+    out_msg->set_md_mono_time(radar_md_mono_time);
+    out_msg->set_car_state_mono_time(car_state_mono_time);
+
+    radar_writer_->Write(out_msg);
+
+    return true;
+}
