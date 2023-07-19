160a62b05d93568960e18d9b3410a8d263569df4
tang <122545505+1121085292@users.noreply.github.com>
2023-07-19 16:27:47 +0800

    Add files via upload

---
+++ camera_detection_component.cc
@@ -0,0 +1,17 @@
+#include "cipv_fusion_component/component/camera_detection_component.h"
+using apollo::cyber::ComponentBase;
+
+bool CameraPm::Init()
+{
+    camera_writer_ = node_->CreateWriter<LeadsV3>("/perception/camera_data/");
+    return true;
+}
+
+bool CameraPm::Proc()
+{
+    auto out_msg = std::make_shared<LeadsV3>();
+
+    radar_writer_->Write(out_msg);
+
+    return true;
+}
---
+++ radar_detection_component.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <memory>
+
+#include "cyber/class_loader/class_loader.h"
+#include "cyber/component/timer_component.h"
+#include "cyber/component/component.h"
+#include "cyber/cyber.h"
+#include "cipv_fusion_component/proto/radar_detection.pb.h"
+
+using cipv_fusion_component::proto::RadarData;
+
+class RadarPm : public apollo::cyber::TimerComponent {
+  public:
+    bool Init() override;
+    bool Proc() override;
+
+  private:
+    std::shared_ptr<apollo::cyber::Writer<RadarData>> radar_writer_ = nullptr;
+
+};
+CYBER_REGISTER_COMPONENT(RadarPm);
