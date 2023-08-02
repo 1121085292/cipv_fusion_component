de65982b54e3e768d6ef21da0501438250a9d3d9
tang <122545505+1121085292@users.noreply.github.com>
2023-08-02 16:59:39 +0800

    Update cipv_fusion_component.cc

--- component/cipv_fusion_component.cc
+++ component/cipv_fusion_component.cc
@@ -7,6 +7,9 @@ using apollo::cyber::ComponentBase;
 bool CipvFusionComponent::Init()
 {   
     // seq_num = 0.0f;
+    current_time = 0;
+    v_ego = 0;
+    ready = false;
     //init publisher
     fusion_writer_ = ComponentBase::node_->CreateWriter<RadarState>("/perception/output/cipv/");
     inner_fusion_writer_ = ComponentBase::node_->CreateWriter<LiveTracks>("/perception/UI/cipv/");
@@ -16,15 +19,29 @@ bool CipvFusionComponent::Init()
 bool CipvFusionComponent::Proc(const std::shared_ptr<RadarData> &radar,
                                 const std::shared_ptr<CarState>& car,
                                 const std::shared_ptr<ModelV2>& camera)
-{
+{   
+    if(car){
+        v_ego = car->v_ego();
+        v_ego_hist.push_back(v_ego);
+    } else {
+        AINFO << "No car state sign";
+        return false;
+    }
 
+    if(camera){
+        ready = true;
+    } else {
+        AINFO << "No camera sign";
+        return false;
+    }
+    current_time = std::max(car->can_mono_time(), camera->timestamp_eof());
     auto out_msg = std::make_shared<RadarState>();
-
+    
     AINFO << "Enter cipv fusion component, message timestamps:"
-        << radar->can_mono_times() << "current timestamps:"
-        << std::max(car->can_mono_times(), camera->timestamp_eof());
+        << radar->can_mono_time() << "current timestamps:"
+        << current_time;
 
-    bool status = InternalProc(radar, car, camera, out_msg);
+    bool status = InternalProc(rr, car, camera, out_msg);
     if (status) {
         fusion_writer_->Write(out_msg);
         AINFO << "Send cipv output message.";
