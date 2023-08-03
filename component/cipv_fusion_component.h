1a9c73ccd9b240d79466ee9e8fadfec2db843201
tang <122545505+1121085292@users.noreply.github.com>
2023-08-03 17:10:43 +0800

    Update cipv_fusion_component.h

--- component/cipv_fusion_component.h
+++ component/cipv_fusion_component.h
@@ -6,17 +6,10 @@
 #include "cyber/component/component.h"
 #include "cyber/cyber.h"
 
-#include "cipv_fusion_component/proto/radar_detection.pb.h"
-#include "cipv_fusion_component/proto/car_state.pb.h"
-#include "cipv_fusion_component/proto/camera_detection.pb.h"
-#include "cipv_fusion_component/proto/cipv_fusion.pb.h"
+#include "cipv_fusion_component/src/kalman_filter.h"
+#include "cipv_fusion_component/src/meta.h"
 
 using apollo::cyber::Component;
-using cipv_fusion_component::proto::ModelV2;
-using cipv_fusion_component::proto::RadarData;
-using cipv_fusion_component::proto::CarState;
-using cipv_fusion_component::proto::RadarState;
-using cipv_fusion_component::proto::LiveTracks;
 
 class CipvFusionComponent : public Component<RadarData, CarState, ModelV2>{
   public:
@@ -37,10 +30,5 @@ class CipvFusionComponent : public Component<RadarData, CarState, ModelV2>{
                     std::shared_ptr<RadarState>& out_msg);
     double LaplacianCdf(double x, double mu, double b);
     
-    uint64_t current_time;
-    double v_ego;
-    std::vector<double> v_ego_hist;
-    bool ready;
-    std::deque<RadarData> radarDataDeque;
 };
 CYBER_REGISTER_COMPONENT(CipvFusionComponent);
