b1ad14f0566a2bcccef74cdfab88739d1c4e7785
tang <122545505+1121085292@users.noreply.github.com>
2023-08-03 17:09:30 +0800

    Update car_state_component.cc

--- component/car_state_component.cc
+++ component/car_state_component.cc
@@ -3,7 +3,7 @@ using apollo::cyber::ComponentBase;
 
 bool CarPm::Init()
 {   
-    speed = 0.0f;
+    // speed = 0.0f;
     car_writer_ = node_->CreateWriter<CarState>("/canbus/car_data/");
     return true;
 }
@@ -12,8 +12,8 @@ bool CarPm::Proc()
 {
     auto out_msg = std::make_shared<CarState>();
     out_msg->set_v_ego(100);
-    // // static uint64_t can_mono_time = 345678;
-    // // out_msg->set_can_mono_time(can_mono_time++);
+    static uint64_t can_mono_time = 345678;
+    out_msg->set_can_mono_time(can_mono_time++);
     car_writer_->Write(out_msg);
 
     return true;
