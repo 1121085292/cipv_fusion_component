117859e23772cfe1a6b5279afe117fda303c7362
tang <122545505+1121085292@users.noreply.github.com>
2023-08-04 14:26:30 +0800

    Update radard.h

--- src/radard.h
+++ src/radard.h
@@ -5,26 +5,9 @@
 
 #include "cipv_fusion_component/src/kalman_filter.h"
 #include "cipv_fusion_component/src/sensor_manager.h"
+#include "cipv_fusion_component/src/track.h"
 
 #include "cipv_fusion_component/src/meta.h"
-
-class Track
-{
-private:
-    /* data */
-public:
-    Track(/* args */);
-    ~Track();
-};
-
-Track::Track(/* args */)
-{
-}
-
-Track::~Track()
-{
-}
-
 
 class RadarD {
 public:
@@ -33,12 +16,9 @@ public:
 
 private:
     double current_time;
-    std::map<int, std::map<std::string, Track>> tracks;
+    std::map<int, Track> tracks;
     KalmanFilter kalman_params;
     double v_ego;
     std::deque<double> v_ego_hist;
     bool ready;
-
-    // 创建unordered_map来存储Radar数据点
-    std::unordered_map<int, RadarPoint> ar_pts;
 };
