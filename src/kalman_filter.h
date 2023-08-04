a2153f82f8c41f50d1bbe7dadb53c255425ac2bd
tang <122545505+1121085292@users.noreply.github.com>
2023-08-04 14:25:51 +0800

    Update kalman_filter.h

--- src/kalman_filter.h
+++ src/kalman_filter.h
@@ -7,8 +7,38 @@ class KalmanFilter {
 public:
     KalmanFilter(double dt);
 
+    std::vector<std::vector<double>> KalmanFilter::GetA() const {
+        return A;
+    }
+
+    std::vector<double> KalmanFilter::GetC() const {
+        return C;
+    }
+
+    std::vector<std::vector<double>> KalmanFilter::GetK() const {
+        return K;
+    }
+
 private:
     std::vector<std::vector<double>> A;
     std::vector<double> C;
     std::vector<std::vector<double>> K;
+};
+
+class KF1D {
+public:
+    KF1D(){};
+    KF1D(const std::vector<std::vector<double>>& x0, const std::vector<std::vector<double>>& A,
+         const std::vector<double>& C, const std::vector<std::vector<double>>& K);
+
+    void Update(const std::vector<double>& meas);
+
+    std::vector<std::vector<double>> GetX() const;
+
+private:
+    std::vector<std::vector<double>> x;
+    std::vector<std::vector<double>> A;
+    std::vector<double> C;
+    std::vector<std::vector<double>> K;
+    std::vector<std::vector<double>> A_K;
 };
