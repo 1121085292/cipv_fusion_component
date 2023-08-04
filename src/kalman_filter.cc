f7db138c9a9e31a299b391c0b4a78ebd2f606564
tang <122545505+1121085292@users.noreply.github.com>
2023-08-04 14:25:34 +0800

    Update kalman_filter.cc

--- src/kalman_filter.cc
+++ src/kalman_filter.cc
@@ -1,6 +1,6 @@
 #include "cipv_fusion_component/src/kalman_filter.h"
 
-double interp(double x, const std::vector<double>& xs, const std::vector<double>& ys) {
+double Interp(double x, const std::vector<double>& xs, const std::vector<double>& ys) {
     if (x <= xs.front()) {
         return ys.front();
     }
@@ -21,20 +21,50 @@ double interp(double x, const std::vector<double>& xs, const std::vector<double>
 KalmanFilter::KalmanFilter(double dt) {
     assert(dt > 0.01 && dt < 0.1 && "Radar time step must be between .01s and 0.1s");
 
-    std::vector<double> dts;
-    std::vector<double> K0;
-    std::vector<double> K1;
+    A = {{1.0, dt}, {0.0, 1.0}};
+    C = {1.0, 0.0};
 
+    // Assuming dt is between 0.01 and 0.1
+    std::vector<double> dts;
     for (int i = 1; i <= 10; ++i) {
         dts.push_back(dt * 0.01 * i);
     }
 
-    // 这里初始化 K0 和 K1 列表
-    K0 = {0.12288, 0.14557, 0.16523, 0.18282, 0.19887, 0.21372, 0.22761, 0.24069, 0.2531, 0.26491};
-    K1 = {0.29666, 0.29331, 0.29043, 0.28787, 0.28555, 0.28342, 0.28144, 0.27958, 0.27783, 0.27617};
+    std::vector<double> K0 = {0.12288, 0.14557, 0.16523, 0.18282, 0.19887, 0.21372, 0.22761, 0.24069, 0.2531, 0.26491};
+    std::vector<double> K1 = {0.29666, 0.29331, 0.29043, 0.28787, 0.28555, 0.28342, 0.28144, 0.27958, 0.27783, 0.27617};
 
-    // 使用线性插值计算 K
-    A = {{1.0, dt}, {0.0, 1.0}};
-    C = {1.0, 0.0};
-    K = {{interp(dt, dts, K0)}, {interp(dt, dts, K1)}};
+    K = {{Interp(dt, dts, K0)}, {Interp(dt, dts, K1)}};
+}
+
+
+KF1D::KF1D(const std::vector<std::vector<double>>& x0, const std::vector<std::vector<double>>& A,
+         const std::vector<double>& C, const std::vector<std::vector<double>>& K){
+     x = x0;
+    this->A = A;
+    this->C = C;
+    this->K = K;
+
+    // Precompute A_K = A - K*C
+    A_K = A;
+    for (size_t i = 0; i < A.size(); ++i) {
+        for (size_t j = 0; j < A[i].size(); ++j) {
+            A_K[i][j] -= K[i][j] * C[j];
+        }
+    }
+}
+
+void KF1D::Update(const std::vector<double>& meas) {
+    // Assuming x and meas have compatible dimensions
+    for (size_t i = 0; i < A_K.size(); ++i) {
+        double new_x_i = 0.0;
+        for (size_t j = 0; j < A_K[i].size(); ++j) {
+            new_x_i += A_K[i][j] * x[j][0];
+        }
+        new_x_i += K[i][0] * meas[0];
+        x[i][0] = new_x_i;
+    }
+}
+
+std::vector<std::vector<double>> KF1D::GetX() const {
+    return x;
 }
