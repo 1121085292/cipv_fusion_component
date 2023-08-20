400f13bf09cf485f6e2e1ade1e8dcad506cfafe4
tangpeng <1121085292@qq.com>
2023-08-20 22:05:26 +0800

    0820

--- component/cipv_fusion_component.cc
+++ component/cipv_fusion_component.cc
@@ -2,21 +2,16 @@
 
 #include "cipv_fusion_component/component/cipv_fusion_component.h"
 
+#include <boost/python.hpp>
+
 using apollo::cyber::ComponentBase;
+
+using namespace boost::python;
 
 bool CipvFusionComponent::Init()
 {   
     // 从底层读数据
-    // AINFO << "radard is waiting for CarParams";
-    // CarParams carParams = CarParams::from_bytes()
-    // 初始化RadarD对象
 
-    // v_ego 初始化为 0.0
-    // v_ego = 0.0;
-    // // 使用 std::deque 作为 v_ego_hist 来存储历史记录
-    // v_ego_hist = std::deque<double>(delay + 1, 0.0);
-    // ready = false;
-    // enable_lead = true;
     //init publisher
     fusion_writer_ = ComponentBase::node_->CreateWriter<RadarState>("/perception/output/cipv/");
     // inner_fusion_writer_ = ComponentBase::node_->CreateWriter<LiveTracks>("/perception/UI/cipv/");
@@ -27,11 +22,6 @@ bool CipvFusionComponent::Proc(const std::shared_ptr<RadarData> &radar,
                                 const std::shared_ptr<CarState>& car,
                                 const std::shared_ptr<ModelV2>& camera)
 {   
-    // double max_time = std::max(car->can_mono_time(), camera->timestamp_eof());
-    // current_time = 1e-9 * max_time;
-    // AINFO << "fusion timestamp: " << current_time;
-
-    // auto out_msg = std::make_shared<RadarState>();
 
     std::shared_ptr<RadarState> out_msg(new (std::nothrow)
                                                        RadarState);
@@ -39,11 +29,37 @@ bool CipvFusionComponent::Proc(const std::shared_ptr<RadarData> &radar,
                                                       LiveTracks);
 
     std::shared_ptr<RadarD> radard = std::make_shared<RadarD>(radar_ts_, 0);
-    bool status = radard->Update(radar, car, camera, out_msg, viz_msg, enable_lead_);
-    // bool status = InternalProc(radar, car, camera, out_msg, viz_msg);
-    if (status) {
-        fusion_writer_->Write(out_msg);
-        AINFO << "Send cipv output message.";
+    // bool status = radard->Update(radar, car, camera, out_msg, viz_msg, enable_lead_);
+
+    Py_Initialize();
+    if(!Py_IsInitialized()){
+        std::cout << "python init failed" << std::endl;
+        return false;
     }
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("sys.path.append('/apollo_workspace/cipv_fusion_component/component/python')");
+
+    PyObject* module = PyImport_ImportModule("hello_world");
+    // boost::python::object module(import("hello_world"));
+    if(module == nullptr){
+        std::cout << "module not found: hello_world" << std::endl;
+        return false;
+    }
+
+    PyObject* func = PyObject_GetAttrString(module, "say");
+    // module.attr("say");
+
+    if(!func || !PyCallable_Check(func)){
+        std::cout << "function not found: say" << std::endl;
+        return false;
+    }
+
+    PyObject_CallObject(func, nullptr);
+
+    Py_Finalize();
+    // if (status) {
+    //     fusion_writer_->Write(out_msg);
+    //     AINFO << "Send cipv output message.";
+    // }
     return true;
 }
---
+++ component/python/print_test.py
@@ -0,0 +1,7 @@
+number_x = 30
+ 
+print ("This is embedded python file")
+ 
+if __name__ == '__main__':
+    number_y = 20
+    print ("Py run itself.")
